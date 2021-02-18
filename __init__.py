3de34a28bae4d563421d86fe78180ecb6cf7af27
matumasadev02 <77044083+matumasadev02@users.noreply.github.com>
2021-02-18 14:23:47 +0900

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,4 +1,11 @@
-from Flask import *
-@app.route("/")
-def helloworld():
-    return("Hello,World")
+# -*- coding: utf-8 -*-
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return 'hello, world'
+
+if __name__ == '__main__':
+    app.run()
